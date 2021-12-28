ba1684c6c28703bedb95ca9970c4fc78c02e322b
SkyXcripto {dev} <producer@skynance.net>
2021-12-28 18:13:47 -0300

    finished Authentication

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -1,18 +1,37 @@
-import React, { createContext, useContext, ReactNode, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react";
 import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Alert } from "react-native";
 
+type User = {
+  id: string;
+  name: string;
+  isAdmin: boolean;
+};
 type AuthContextData = {
   signIn: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
+  forgotPassword: (email: string) => Promise<void>;
   isLogging: boolean;
+  user: User | null;
 };
 type AuthProviderProps = {
   children: ReactNode;
 };
 
+const USER_COLLECTION = "@gopizza:users";
+
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
   const [isLogging, setIsLogging] = useState(false);
 
   async function signIn(email: string, password: string) {
@@ -24,12 +43,35 @@ function AuthProvider({ children }: AuthProviderProps) {
     auth()
       .signInWithEmailAndPassword(email, password)
       .then((account) => {
-        console.log(account);
+        firestore()
+          .collection("users")
+          .doc(account.user.uid)
+          .get()
+          .then(async (profile) => {
+            const { name, isAdmin } = profile.data() as User;
+
+            if (profile.exists) {
+              const userData = {
+                id: account.user.uid,
+                name,
+                isAdmin,
+              };
+
+              await AsyncStorage.setItem(
+                USER_COLLECTION,
+                JSON.stringify(userData)
+              );
+              setUser(userData);
+            }
+          })
+          .catch(() => {
+            Alert.alert("Login", "Erro ao carregar o perfil do usuário.");
+          });
       })
       .catch((error) => {
         const { code } = error; // pega o código do erro
         if (code === "auth/user-not-found" || code === "auth/wrong-password") {
-          return Alert.alert("Login", "E-mail ou usuário não encontrado!");
+          return Alert.alert("Login", "E-mail ou senha incorreta!");
         } else {
           return Alert.alert("Login", "Erro ao fazer login!");
         }
@@ -37,10 +79,58 @@ function AuthProvider({ children }: AuthProviderProps) {
       .finally(() => setIsLogging(false));
   }
 
+  async function loadUserStorageData() {
+    setIsLogging(true);
+
+    const storagedUser = await AsyncStorage.getItem(USER_COLLECTION);
+
+    if (storagedUser) {
+      const userData = JSON.parse(storagedUser) as User;
+      console.log(userData);
+      setUser(userData);
+    }
+
+    setIsLogging(false);
+  }
+
+  async function signOut() {
+    await auth().signOut();
+    await AsyncStorage.removeItem(USER_COLLECTION);
+    setUser(null);
+  }
+
+  async function forgotPassword(email: string) {
+    if (!email) {
+      return Alert.alert("Redefinir minha senha", "Informe o E-mail.");
+    }
+
+    auth()
+      .sendPasswordResetEmail(email)
+      .then(() =>
+        Alert.alert(
+          "Redefinir minha senha",
+          "Enviamos um link no seu e-mail para redefinir sua senha."
+        )
+      )
+      .catch(() =>
+        Alert.alert(
+          "Redefinir minha senha",
+          "Não foi possível enviar o e-mail para redefinir a senha."
+        )
+      );
+  }
+
+  useEffect(() => {
+    loadUserStorageData();
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
         signIn,
+        signOut,
+        forgotPassword,
+        user,
         isLogging,
       }}
     >
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -17,10 +17,14 @@ import { useAuth } from "@hooks/Auth";
 export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { signIn, isLogging } = useAuth();
+  const { signIn, signOut, forgotPassword, isLogging } = useAuth();
 
   function handleSignIn() {
     signIn(email, password);
+  }
+
+  function handleforgotPassword() {
+    forgotPassword(email);
   }
 
   return (
@@ -46,7 +50,7 @@ export function SignIn() {
             onChangeText={setPassword}
           />
 
-          <ForgotPasswordButton>
+          <ForgotPasswordButton onPress={handleforgotPassword}>
             <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
           </ForgotPasswordButton>
 
