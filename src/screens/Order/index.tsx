ff30e4a6ebb4220024b9735190946c8c0a1d66ed
SkyXcripto {dev} <producer@skynance.net>
2021-12-30 12:09:38 -0300

    Create Form Pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,30 +1,68 @@
 import React, { useState } from "react";
+
 import { Platform } from "react-native";
 
-import { Container, Header, Photo, Sizes } from "./styles";
+import {
+  Container,
+  ContentScroll,
+  Header,
+  Photo,
+  Sizes,
+  Form,
+  Title,
+  Label,
+  InputGroup,
+  Price,
+  FormRow,
+} from "./styles";
+
 import { PIZZA_TYPES } from "@utils/pizzaTypes";
 import { ButtonBack } from "@components/ButtonBack";
 import { RadioButton } from "@components/RadioButton";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function Order() {
   const [size, setSize] = useState("");
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-      <Header>
-        <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
-      </Header>
-      <Photo source={{ uri: "https://github.com/skyxcripto.png" }} />
+      <ContentScroll>
+        <Header>
+          <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
+        </Header>
+        <Photo source={{ uri: "https://github.com/skyxcripto.png" }} />
 
-      <Sizes>
-        {PIZZA_TYPES.map((item) => (
-          <RadioButton
-            key={item.id}
-            title={item.name}
-            onPress={() => setSize(item.id)}
-            selected={size === item.id}
-          />
-        ))}
-      </Sizes>
+        <Form>
+          <Title>Nome da Pizza</Title>
+          <Label>Selecione o tamanho</Label>
+
+          <Sizes>
+            {PIZZA_TYPES.map((item) => (
+              <RadioButton
+                key={item.id}
+                title={item.name}
+                onPress={() => setSize(item.id)}
+                selected={size === item.id}
+              />
+            ))}
+          </Sizes>
+
+          <FormRow>
+            <InputGroup>
+              <Label>Numero da Mesa</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+          </FormRow>
+
+          <Price>Valor de R$ 00,00</Price>
+          <Button title="Confirmar Pedido " isLoading={false} />
+        </Form>
+      </ContentScroll>
     </Container>
   );
 }
